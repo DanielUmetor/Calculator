ff081b12c0d0fcca7dcf83f408ead9e421900f12
DanielUmetor <dnumetor@gmail.com>
2024-05-20 11:03:39 +0200

    calculator

--- script/script.js
+++ script/script.js
@@ -1,15 +1,30 @@
-function appendToDisplay(value) {
-    document.getElementById("display").value += value;
-}
+let sum = '';
 
-function calculate() {
-    try {
-        document.getElementById("display").value = eval(document.getElementById("display").value);
-    } catch (error) {
-        document.getElementById("display").value = "Error";
+function appendToDisplay(value) {
+    const output = document.getElementById("output");
+    if (value === '.') {
+        if (!sum.includes('.')) {
+            sum += value;
+            output.textContent = sum;
+        }
+    } else {
+        sum += value;
+        output.textContent = sum;
     }
 }
 
-function clearDisplay() {
-    document.getElementById("display").value = "";
+function evaluateSum() {
+    const output = document.getElementById("output");
+    try {
+        sum = eval(sum).toString();
+        output.textContent = sum;
+    } catch (error) {
+        output.textContent = "Error";
+    }
+}
+
+function clearSum() {
+    const output = document.getElementById("output");
+    sum = '';
+    output.textContent = '';
 }
