59fd2474a1fbdfe2c30cebeadc28839837862c08
DanielUmetor <dnumetor@gmail.com>
2024-05-20 09:54:08 +0200

    calculator

--- css/script/code.js
+++
@@ -1,4 +0,0 @@
-let dataEntry = document.querySelector ('[data-entry]') // used to get the data reference
-function display (value) {  // (display is the funtion name),
-    dataEntry.value += value
-}
